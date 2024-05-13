8f147287e7879f142bea1a1656c4c5d54312e82c
Rahul Godiyal <rgodiyal482@gmail.com>
2024-05-13 21:47:06 +0530

    RAHULG - Todo app

--- PHP-Basics/todo.php
+++ PHP-Basics/todo.php
@@ -1,44 +1,37 @@
-<?php
-require 'todo.html';
-class Todo 
-{
-    private ?string $taskname;
-    private array $tasklist;
+<?php require_once('TodoCtrl.php'); ?>
 
-    
-    public function __construct() {
-        session_start();
-        $this->saveTasks();
-       
-    }
+<!DOCTYPE html>
+<html lang="en">
 
-    public function saveTasks() {
-       $_SESSION["taskName"] = array();
-        $this->tasklist[] = $_POST["todoname"];
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Todo App</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
 
-        if( $_SERVER["REQUEST_METHOD"] == "POST" ){
-            $_SESSION["taskName"] = $_POST["todoname"];
-            
-        }
-        $this->showTasks();
-    }
+<body>
+    <div class="container text-center">
+        <h1>Todo App</h1>
+        <div class="container">
+            <form method="post" class="row align-items-center">
+                <div class="col-auto">
+                    <input type="text" name="todo" class="form-control" placeholder="Enter new todo...">
+                </div>
+                <div class="col-auto">
+                    <input type="submit" name="save" value="Save" class="btn btn-primary">
+                </div>
+            </form>
 
-    public function getTasks($taskname) :self {
-       
-        $this->tasklist[] = $taskname;
-        return $this;
-    }
+            <?php foreach ($todoCtrl->getTodos() as $todo) { ?>
+                <div class="alert alert-dark" role="alert">
+                    <?= $todo; ?>
+                </div>
+            <?php } ?>
+        </div>
 
-    public function showTasks()  {
-        // $sessionData = $_SESSION["taskName"];
-        // foreach($sessionData as $session)
-        // {
-            print_r($_SESSION["taskName"]);
-        // }
-       
-    }
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
 
-}
-
-$obj = new Todo();
-?>
+</html>
